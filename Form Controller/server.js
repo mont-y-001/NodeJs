178cfb565152bf4cf04c159d8660dce1718655ea
Mohit Yadav <my266620@gmail.com>
2025-10-25 23:28:17 +0530

    Drive Start

--- Form Controller/server.js
+++ Form Controller/server.js
@@ -38,8 +38,10 @@ app.get('/register',(req,res)=>{
 })
 
 app.post('/register', async (req,res)=>{
-   const {username, email, password} = req.body;
+   const {username, email, password} = req.body;    //This is De-Structuring to store data in req.body
     
+
+   //await work as it pause the execution of async function means first await function run then will rest code execute
    let newUser = await userModel.create({
     username:username,
     email: email,
@@ -49,5 +51,36 @@ app.post('/register', async (req,res)=>{
     res.send(newUser);
 })
 
+
+//##READ OPERATION
+app.get('/get-user',(req,res)=>{
+    userModel.findOne({                  
+//#Find => If no element found it will return [] (empty array)   
+//#FindOne=> if no element or data found it will return null and in case of same name of user it will return 1st user in all of them
+        username:'Mohit Yadav'
+    }).then((user)=>{
+        console.log(user)
+        res.send(user);
+    })
+})
+
+//##UPDATE OPERATION
+app.get('/update-form', async (req,res)=>{      //async and await must to perform updation
+    await userModel.findOneAndUpdate({
+        username : "Monty yadav"    //if you are searching on the basis of username be sure that it must be correct(spelling,no extra space)
+    },{
+        email:"bhuttayadav123@gmail.com"    //updated email
+    })
+    res.send("User Updated");
+})
+
+
+//##Delete OPERATION
+app.get('/delete-user', async (req,res)=>{
+    await userModel.findOneAndDelete({
+        username : "Mohit Yadav"
+    })
+    res.send("User Deleted")
+})
 
 app.listen(3000);
