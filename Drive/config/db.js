c58cefdeb117925f9e006279d352c2afb6320bf4
Mohit Yadav <my266620@gmail.com>
2025-11-02 18:14:06 +0530

    .

--- Drive/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose') ;
-
-function connectToDB(){
-    mongoose.connect(process.env.MONGO_URI).then(() =>{
-        console.log('Connected to DB')
-    })
-}
-module.exports = connectToDB
